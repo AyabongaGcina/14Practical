06795e66b3cb3950c3778de13ed5c6eebee8bce5
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-04 13:18:45 +0000

    implemented the node class

--- OpenHash.java
+++ OpenHash.java
@@ -1,1 +1,10 @@
-public lass
+public class OpenHash{
+    private static class Node{
+        int key;
+        String value; 
+        Node(int key,String value){
+            this.key=key;
+            this.value=value;
+        }
+    }
+}
