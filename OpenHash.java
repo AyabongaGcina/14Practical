5ba9d039ff5b07c44e63360931d1877d9fe1ff2b
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-04 13:21:08 +0000

    Create the hash table array

--- OpenHash.java
+++ OpenHash.java
@@ -6,5 +6,20 @@ public class OpenHash{
             this.key=key;
             this.value=value;
         }
+
     }
+     // The hash table array
+    private Node[] table;  
+    private int m;          // Size of the table
+    private int size;       // Number of items inserted
+
+    // Constructor
+    public OpenHash(int tableSize) {
+        this.m = tableSize;
+        this.table = new Node[m + 1];  // indices 1 to m (ignore index 0)
+        this.size = 0;
+    }
+
+
+
 }
