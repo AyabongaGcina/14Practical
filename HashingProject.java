779d59c056a3d32cb49b38fff3891a81367e122f
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-04 12:36:03 +0000

    Started the main method

--- HashingProject.java
+++ HashingProject.java
@@ -2,3 +2,9 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.Random;
+
+public class HashingProject{
+    pubic static void main(String[]args){
+        
+    }
+}
